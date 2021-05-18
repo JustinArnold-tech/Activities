96844a2312778932c9d3ed49523bd7c67b395efb
Justin Arnold <justinarnold@Justins-MacBook-Pro.local>
2021-05-18 14:39:06 -0600

    Activity7-JustinArnold

---
+++ Activitiy7-Justin.arnold.py
@@ -0,0 +1,24 @@
+def add(N1, N2):
+    answer1 = N1 + N2
+    print(answer1)
+    return answer1
+
+def subtracting(N1, N2):
+    answer2 = N1 - N2
+    print(answer2)
+    return answer2
+
+def Multiplying(N1, N2):
+    answer3 = N1 * N2
+    print(answer3)
+    return answer3
+
+def Dividing(N1, N2):
+    answer4 = N1 / N2
+    print(answer4)
+    return answer4
+
+add(4, 3)
+subtracting(4, 3)
+Multiplying(4,3)
+Dividing(4, 3)
