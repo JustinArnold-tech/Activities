433dc745a44a75417d03e559022ea451c1693295
Justin Arnold <justinarnold@Justins-MacBook-Pro.local>
2021-05-19 13:07:21 -0600

    Activity8-JustinArnold

---
+++ Activity8-JustinArnold.py
@@ -0,0 +1,20 @@
+x = 21
+y = 23
+z = 19
+
+if(x > y) and (y > z):
+    print(x)
+if(x < y) and (y > z):
+    print(y)
+if(x > y) and (y < z):
+    print(z)
+
+
+Light = "Green"
+
+if(Light == "Green"):
+    print("Change light from green to yellow")
+elif(Light == "Yellow"):
+    print("Change light from yellow to red")
+else:
+    print("Change light from red to green")
---
+++ Comparison.py
@@ -0,0 +1,125 @@
+#Arithmetic Operators:
+    #"+, -, *, /"
+#Practice with Comparison Operator:
+
+#So far the operators we have covered are:
+#">" -> Greater Than
+#"<" -> Less Than
+#"==" -> Eguals (Hard Equality)
+#">=" -> Greater than or Equal
+#"<=" -> Less than or Equal
+
+#Comparison Opertors Result in a Boolean Value
+
+# sample1 = 4 > 9
+# sample2 = 2 < 4
+
+# print(sample1)
+# print(sample2)
+
+#Practice with Logical Operators:
+# soulution = False and False
+# print(soulution)
+
+#Practicing with Control Flow or If Statements:
+#Step 1: Write the keyword "if"
+#Step 2: follow it with parentheses
+#Step 3: Put a Colon at the end
+
+# if(True):
+#     name = "Justin"
+#     print("Hellow World")
+#     print("Justin")
+#     print("Justin".lower())
+
+
+# expression1 = 23 > 3
+# expression2 = 98 < 200
+# expression3 = "Justin" > "Ethereum"
+
+# if(expression3):
+#     print("Elon Musk messed up!")
+
+
+
+
+#Creating website: If you sign up, you provide some username and password get stored in some database owned by that company you signed up for.
+#When you try to log in AFTER you sign up. The process looks literally like this:
+    #if username passed in IS EQUAL TO username in database provided at sign up:
+        #Provide them their page
+
+#Else practice. The ELSE block only runs if the "if" block doesn't.
+#The IF BLOCK doesnt run if the exp
+
+# expression1 = 23 > 3
+# expression2 = 98 < 200
+# expression3 = "Justin" > "Ethereum"
+
+# if(expression3):
+#     print("Elon Musk messed up!")
+# else:
+#     print("Elon Musk didn't mess up after all!")
+
+#Elif practice.
+
+# expression1 = 23 > 3
+# expression2 = 98 < 200
+# expression3 = "Justin" > "Ethereum"
+
+# if(expression3):
+#     print("Elon Musk messed up!")
+# else:
+#     print("Elon Musk didn't mess up after all!")
+
+#Now we are working with chained conditionals:
+    #WHCIH 
+
+# expression1 = 7 == 7
+# expression2 = 7 != 7
+# expression3 = False
+# expression4 = "Justin" == "justin"
+# expression5 = "Justin" == "Justin"
+
+
+# if(expression2):
+#     print("I'm in the IF Statement WOO!")
+# elif(expression3):
+#     print("I'm in the ELIF statement WOO!")
+# elif(expression4):
+#     print("I'm, on")
+# else:
+#     print("Boo, I'm in the ELSE statment. This means the other two conditions HAVE to have failed....:(")
+
+
+#ShortHand
+# a = 10
+# b = 4
+
+# if a > b: print("A is greater") 
+# else print("B is greater")
+
+#Now we are working with Nested IF/Else Statements:
+if(True):
+    print("Hi")
+    if(True):
+        print("What it do")
+    elif(False):
+        print("KLK")
+    else:
+        print("What's up")
+elif():
+    if():
+        print("Stuff1")
+    elif(False):
+        print("Stuff2")
+    else:
+        print("Stuff3")
+else:
+    if(True):
+        print("bye")
+    elif(True):
+        print("peace")
+    else:
+        print("duces")
+
+
