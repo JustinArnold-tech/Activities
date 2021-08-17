3d57a8f42b1283ca45f0f811358df328dcf00b02
JustinArnold-tech <79416942+JustinArnold-tech@users.noreply.github.com>
2021-08-17 11:06:49 -0600

    Add files via upload

--- Dice/dice.js
+++ Dice/dice.js
@@ -1,11 +1,15 @@
- rng = []
- document.querySelector("#button1").addEventListener("click", () => { 
+player1 = 0
+
+document.querySelector("#button1").addEventListener("click", () => { 
     var rng1 = Math.floor(Math.random() * 6) + 1;
+    var player1Score = 0
+    player1Score = player1Score + rng1;
+    player1 = player1Score
     
     if(rng1 === 1){
         document.querySelector("#dice1").src = "https://etc.usf.edu/clipart/42100/42158/die_01_42158_lg.gif";
         document.querySelector("#DH1").innerText = "ONE";
-        rng.append(rng1);        
+           
     }else if(rng1 === 2){
         document.querySelector("#dice1").src = "https://etc.usf.edu/clipart/42100/42159/die_02_42159_lg.gif";
         document.querySelector("#DH1").innerText = "TWO";
@@ -25,13 +29,20 @@
     }else if(rng1 === 6){
         document.querySelector("#dice1").src = "https://etc.usf.edu/clipart/42100/42164/die_06_42164_lg.gif";
         document.querySelector("#DH1").innerText = "SIX";
-        
- 
     } 
+    console.log(player1+player2)
+    
 });
+console.log(player1)
 
+
+
+player2 = 0
 document.querySelector("#button2").addEventListener("click", () => { 
     var rng2 = Math.floor(Math.random() * 6) + 1;
+    var player2Score = 0
+    player2Score = player2Score + rng2;
+    player2 = player2Score
     
     if(rng2 === 1){
         document.querySelector("#dice2").src = "https://etc.usf.edu/clipart/42100/42158/die_01_42158_lg.gif";
@@ -60,9 +71,10 @@ document.querySelector("#button2").addEventListener("click", () => {
 
 
     } 
+    document.querySelector("#DH3").innerText = Total =(player1+player2)
 });
 
-console.log(rng) 
+ 
     
     
     
